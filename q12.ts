6ffb3e2bd602a4ee7dc8fe83fbc5a913b6037a00
Sarwat Haroon <159713603+Sarwatharoon68@users.noreply.github.com>
2024-02-25 10:46:55 +0500

    Add files via upload

---
+++ q12.ts
@@ -0,0 +1,10 @@
+/* Ques # 12:
+Greetings: start with the array you used in the exercise 11 but instead of just printing each person's name, print a message to them.The text of each message should be the same , but each message should be personalized with person's name.*/
+
+let friends :string[] = ["Sarwat","Sara","Zoha","Uzma"]
+
+let message :string ="How are you  doing?"
+console.log(friends[0] +" " + message );
+console.log(friends[1] +" " + message );
+console.log(friends[2] +" " + message );
+console.log(friends[3] +" " + message );
